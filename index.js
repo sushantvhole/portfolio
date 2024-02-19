8f31918de24f8875fd8da22f8aece1fa8bcb0c10
Sushant Hole <98330890+sushantvhole@users.noreply.github.com>
2024-02-20 00:28:58 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const hamburger=document.querySelector(".hamburger");
-const navlinks=document.querySelector(".links");
-
-hamburger.addEventListener("click",()=>{
-    hamburger.classList.toggle("active");
-    navlinks.classList.toggle("active")
-})
